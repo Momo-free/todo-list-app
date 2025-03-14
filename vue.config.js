f238a37c6b27d7d2d918d91d3c0a405b250ea46d
dao-ye-has-realized <3020107152@qq.com>
2025-03-14 11:35:20 +0800

    11111

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,11 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  outputDir: 'docs',
+  publicPath: '/ToDoList/'
 })
-module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/<todo-list-app>/'
-    : '/'
-}
+// module.exports = {
+//   publicPath: process.env.NODE_ENV === 'production'
+//     ? '/<todo-list-app>/'
+//     : '/'
+// }
